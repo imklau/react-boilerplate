331cbed09c1fedecc49bfb87ab31d962d7694429
Noeemi <klaudiazawodnik@gmail.com>
2021-03-27 15:00:21 +0100

    chore: create a start script

--- bin/start.js
+++ bin/start.js
@@ -1,105 +1,90 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 const fs = require("fs-extra")
 const path = require("path")
-const https = require("https")
 const { exec } = require("child_process")
+const editJsonFile = require("edit-json-file")
 
-const packageJson = require("../package.json")
+const rootPackageJson = editJsonFile(
+  path.join(path.join(__dirname, ".."), "package.json")
+)
+const projectName = process.argv.slice(2)[0]
+const scripts = rootPackageJson.get("scripts")
 
-const scripts = `"start": "webpack serve -c ./webpack.dev.js",
-"build": "webpack -c ./webpack.prod.js"`
+if (!projectName) {
+  console.log("⁉️ You need to specify your project name.")
+} else {
+  const getDeps = (deps) => {
+    const obj = deps
+    const depsToRemove = ["fs-extra", "edit-json-file"]
+    depsToRemove.map((item) => delete obj[item])
 
-const getDeps = (deps) =>
-  Object.entries(deps)
-    .map((dep) => `${dep[0]}@${dep[1]}`)
-    .toString()
-    .replace(/,/g, " ")
-    .replace(/^/g, "")
-    // exclude the dependency only used in this file, nor relevant to the boilerplate
-    .replace(/fs-extra[^\s]+/g, "")
+    return Object.entries(obj)
+      .map((dep) => `${dep[0]}@${dep[1]}`)
+      .toString()
+      .replace(/,/g, " ")
+      .replace(/^/g, "")
+  }
 
-console.log("✨ Initializing project..")
+  console.log("✨ Initializing project...")
 
-// create folder and initialize yarn
-exec(
-  `mkdir ${process.argv[2]} && cd ${process.argv[2]} && yarn init -y`,
-  (initErr, initStdout, initStderr) => {
-    if (initErr) {
-      console.error(`Everything was fine, then it wasn't:
-    ${initErr}`)
-      return
-    }
-    const packageJSON = `${process.argv[2]}/package.json`
-    // replace the default scripts
-    fs.readFile(packageJSON, (err, file) => {
-      if (err) throw err
-      console.log("file", file.toString())
+  // create folder and initialize yarn
+  exec(
+    `mkdir ${projectName} && cd ${projectName} && yarn init -y`,
+    (initErr) => {
+      if (initErr) {
+        console.error(`Everything was fine, then it wasn't:${initErr}`)
+        return
+      }
 
-      const data = file
-        .toString()
-        .replace(
-          '"test": "echo \\"Error: no test specified\\" && exit 1"',
-          scripts
+      const packageJson = editJsonFile(`${projectName}/package.json`)
+
+      packageJson.set("scripts", { ...scripts })
+      packageJson.save()
+
+      const filesToCopy = [
+        "webpack.dev.js",
+        "webpack.prod.js",
+        ".babelrc",
+        ".eslintrc",
+        ".gitignore",
+        ".prettierrc",
+        ".stylelintrc",
+      ]
+
+      for (let i = 0; i < filesToCopy.length; i += 1) {
+        fs.createReadStream(path.join(__dirname, `../${filesToCopy[i]}`)).pipe(
+          fs.createWriteStream(`${projectName}/${filesToCopy[i]}`)
         )
-      fs.writeFile(packageJSON, data, (err2) => err2 || true)
-    })
+      }
 
-    const filesToCopy = ["webpack.dev.js", "webpack.prod.js"]
+      // installing dependencies
+      console.log("Installing dependencies -- it might take a few minutes...")
+      const devDeps = getDeps(rootPackageJson.get("devDependencies"))
+      const deps = getDeps(rootPackageJson.get("dependencies"))
 
-    for (let i = 0; i < filesToCopy.length; i += 1) {
-      fs.createReadStream(path.join(__dirname, `../${filesToCopy[i]}`)).pipe(
-        fs.createWriteStream(`${process.argv[2]}/${filesToCopy[i]}`)
+      exec(
+        `cd ${projectName} && git init && node -v && yarn -v && yarn add -D ${devDeps} && yarn add ${deps}`,
+        (npmErr, npmStdout) => {
+          if (npmErr) {
+            console.error(`Some error while installing dependencies ${npmErr}`)
+            return
+          }
+          console.log(npmStdout)
+          console.log("Dependencies installed")
+
+          console.log()
+          console.log("Copying additional files...")
+          // copy the src files
+          fs.copy(path.join(__dirname, "../src"), `${projectName}/src`)
+            .then(() =>
+              console.log(
+                `✅  All done!\n\nYour project is now ready\n\nUse the below command to run the app.\n\ncd ${projectName}\nyarn start`
+              )
+            )
+            .catch((err) => console.error(err))
+        }
       )
     }
-
-    // npm will remove the .gitignore file when the package is installed, therefore it cannot be copied, locally and needs to be downloaded. Use your raw .gitignore once you pushed your code to GitHub.
-    https.get(
-      "https://raw.githubusercontent.com/Nikhil-Kumaran/reactjs-boilerplate/master/.gitignore",
-      (res) => {
-        res.setEncoding("utf8")
-        let body = ""
-        res.on("data", (data) => {
-          body += data
-        })
-        res.on("end", () => {
-          fs.writeFile(
-            `${process.argv[2]}/.gitignore`,
-            body,
-            { encoding: "utf-8" },
-            (err) => {
-              if (err) throw err
-            }
-          )
-        })
-      }
-    )
-
-    console.log("yarn init -- done\n")
-
-    // installing dependencies
-    console.log("Installing deps -- it might take a few minutes..")
-    const devDeps = getDeps(packageJson.devDependencies)
-    const deps = getDeps(packageJson.dependencies)
-    exec(
-      `cd ${process.argv[2]} && git init && node -v && yarn -v && yarn add -D ${devDeps} && yarn add -S ${deps}`,
-      (npmErr, npmStdout, npmStderr) => {
-        if (npmErr) {
-          console.error(`Some error while installing dependencies ${npmErr}`)
-          return
-        }
-        console.log(npmStdout)
-        console.log("Dependencies installed")
-
-        console.log("Copying additional files..")
-        // copy additional source files
-        fs.copy(path.join(__dirname, "../src"), `${process.argv[2]}/src`)
-          .then(() =>
-            console.log(
-              `All done!\n\nYour project is now ready\n\nUse the below command to run the app.\n\ncd ${process.argv[2]}\nnpm start`
-            )
-          )
-          .catch((err) => console.error(err))
-      }
-    )
-  }
-)
+  )
+}
